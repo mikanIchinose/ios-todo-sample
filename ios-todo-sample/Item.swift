531668e5b8936436a3ff240786679dbe3b69ba63
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-03-18 00:44:07 +0900

    Initial Commit

---
+++ ios-todo-sample/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ios-todo-sample
+//
+//  Created by 一瀬喜弘 on 2025/03/18.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ios-todo-sampleTests/ios_todo_sampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_todo_sampleTests.swift
+//  ios-todo-sampleTests
+//
+//  Created by 一瀬喜弘 on 2025/03/18.
+//
+
+import Testing
+@testable import ios_todo_sample
+
+struct ios_todo_sampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
